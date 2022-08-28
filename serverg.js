c138a02e8c2707ba3a17bfbd3e3e0d2ecfd635e6
IgorTrp <itrpkovic12920rn@raf.rs>
2022-08-28 23:49:07 +0200

    init

---
+++ serverg.js
@@ -0,0 +1,37 @@
+const express =require("express");
+const app=express();
+const path=require("path");
+const cors = require('cors');
+const history = require('connect-history-api-fallback');
+
+
+app.use(cors({ origin: true, credentials: true }));
+app.use('/admin/',express.static(path.join(__dirname,"static")));
+const index=require(path.join(__dirname,'index.js'));
+const login=require(path.join(__dirname,'login.js'));
+
+const adresa=require(path.join(__dirname,'adresa.js'));
+const proizvod=require(path.join(__dirname,'proizvod.js'));
+const primalac=require(path.join(__dirname,'primalac.js'));
+const porudzbina=require(path.join(__dirname,'porudzbina.js'));
+const korisnik=require(path.join(__dirname,'korisnik.js'));
+const KorisnikPorudzbina=require(path.join(__dirname,'korisnik-porudzbina.js'));
+ 
+
+app.use('/admin/index',index);
+app.use('/admin/login',login);
+
+app.use('/admin/adresa',adresa);
+app.use('/admin/proizvod',proizvod);
+app.use('/admin/primalac',primalac);
+app.use('/admin/porudzbina',porudzbina);
+app.use('/admin/korisnik',korisnik);
+app.use('/admin/korisnik-porudzbina',KorisnikPorudzbina);
+
+const staticMdl =express.static(path.join(__dirname,'dist'));
+
+app.use(staticMdl);
+app.use(history({index:'/index.html'}));
+app.use(staticMdl);
+
+app.listen(9000);
---
+++ static/public/JS/porudzbinaf.js
@@ -0,0 +1,139 @@
+function init() {
+
+    document.getElementById('izbrisi').addEventListener('click', e => {
+        e.preventDefault();
+
+        const data = {id: document.getElementById('iddelete').value};
+        document.getElementById('iddelete').value = '';
+
+        if(overiIO(data.id)){
+        fetch('http://localhost/admin/porudzbina', {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*'},
+            credentials: 'include',
+            body: JSON.stringify(data)
+        }).then(res=>{
+            if(res.status!=400 && res.status!=500)
+                document.location.reload();
+            else{
+                alert("Greska u unosa podataka ili upita");}
+            });
+        }
+        else{
+            alert("Nije dobar unos");
+        }
+    });
+
+    document.getElementById('izmeni').addEventListener('click', e => {
+        e.preventDefault();
+
+        const data = {
+        id: document.getElementById('iid').value,
+        proizvodId: document.getElementById('iproizvodId').value,
+        naznake: document.getElementById('inaznake').value,
+        status: document.getElementById('istatus').value,
+        vremeNastanka: document.getElementById('ivremeNastanka').value,
+        korisnikId: document.getElementById('ikorisnikId').value
+        };
+
+        document.getElementById('iid').value='';
+        document.getElementById('iproizvodId').value='';
+        document.getElementById('inaznake').value='';
+        document.getElementById('istatus').value='';
+        document.getElementById('ivremeNastanka').value='';
+        document.getElementById('ikorisnikId').value='';
+
+        if(overiIO(data.id) && overiI(data.proizvodId) && overiT(data.naznake)&& overiT(data.status)&& overiT(data.vremeNastanka)&& overiI(data.korisnikId)){
+        fetch('http://localhost/admin/porudzbina', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*'},
+            credentials: 'include',
+            body: JSON.stringify(data)
+        }).then(res=>{
+            if(res.status!=400 && res.status!=500)
+                document.location.reload();
+            else{
+                alert("Greska u unosa podataka ili upita");}
+            });
+        }
+        else{
+            alert("Nije dobar unos");
+        }
+    });
+
+
+    document.getElementById('posalji').addEventListener('click', e => {
+        e.preventDefault();
+
+        const data = {
+        id: document.getElementById('id').value,
+        proizvodId: document.getElementById('proizvodId').value,
+        naznake: document.getElementById('naznake').value,
+        status: document.getElementById('status').value,
+        vremeNastanka: document.getElementById('vremeNastanka').value,
+        korisnikId: document.getElementById('korisnikId').value
+        };
+
+        document.getElementById('id').value='';
+        document.getElementById('proizvodId').value='';
+        document.getElementById('naznake').value='';
+        document.getElementById('status').value='';
+        document.getElementById('vremeNastanka').value='';
+        document.getElementById('korisnikId').value='';
+
+
+        if(overiIO(data.id) && overiIO(data.proizvodId) && overiTO(data.naznake)&& overiTO(data.status)&& overiTO(data.vremeNastanka)&& overiIO(data.korisnikId)){
+        fetch('http://localhost/admin/porudzbina', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*'},
+            credentials: 'include',
+            body: JSON.stringify(data)
+        }).then(res=>{
+            if(res.status!=400 && res.status!=500)
+                document.location.reload();
+            else{
+                alert("Greska u unosa podataka ili upita");}
+            });
+        }
+        else{
+            alert("Nije dobar unos");
+        }
+    });
+
+
+
+    fetch('http://localhost/admin/porudzbina', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*'},
+            credentials: 'include',
+        }).then( res => res.json())
+        .then( data => {
+            let lsta = document.getElementById('lista');
+            if(typeof data[0] !=='undefined')
+            data[0].forEach( el => {
+                lsta.innerHTML += `<li>ID: ${el.Id}, proizvod Id: ${el.ProizvodId}, naznake: ${el.Naznake}, status: ${el.Status}, Vreme Nastanka: ${el.VremeNastanka}, Korisnik Id: ${el.KorisnikId}</li>`;
+            });
+        });
+
+
+}
+
+
+
+function overiI(br){
+  //  let br=document.getElementById(id).value 
+    return Number(br) ||  br === '';
+}
+function overiT(br,len=50){
+   // let br=document.getElementById(id).value 
+    return br.length<len ||  br === '';
+}
+
+function overiIO(br){
+  //  let br=document.getElementById(id).value;
+    return Number(br) && typeof br !== 'undefined';
+}
+function overiTO(br,len=50){
+   // let br=document.getElementById(id).value;
+    return br.length<len && typeof br !== 'undefined';
+}
